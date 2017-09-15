13aad1ef139f7d2ee26b9d0e47225259e9447d11
11607984 <miguelcarlos.rodriguez@benilde.edu.ph>
2017-09-15 12:00:36 +0800

    commit

---
+++ mybookstore/App_Code/BillGates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;
+namespace mybookstore.App_Code
+{
+    public class BillGates
+    {
+        public static string GetCon()
+        {
+            return ConfigurationManager.ConnectionStrings["MyCon"].ConnectionString;
+        }
+    }
+}
---
+++ mybookstore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mybookstore.Startup))]
+namespace mybookstore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
